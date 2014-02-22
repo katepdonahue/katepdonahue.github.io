fc97ae3d05794f306b420696dbfead5ceefadd1e
Kate <katepdonahue@gmail.com>
2014-02-22 13:34:36 -0500

    Add routes

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,21 @@ var portfolioApp = angular.module('portfolioApp', [
   'portfolioControllers'
   ]);
 
+portfolioApp.config([
+  '$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'app/partials/home.html',
+      controller: 'HomeCtrl'
+    }).
+    when('/about', {
+      templateUrl: 'app/partials/about.html',
+      controller: 'AboutCtrl'
+    }).
+    when('/portfolio', {
+      templateUrl: 'app/partials/portfolio.html',
+      controller: 'MyPortfolioCtrl'
+    }).
+    otherwise({redirectTo: '/'});
+  }]);
